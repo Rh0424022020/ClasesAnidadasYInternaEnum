6b3ba57999049a5f85af9fd28ce37e417470b29b
LuCy <rh0424022020@unab.edu.sv>
2022-10-15 21:22:14 -0600

    commit
    Se trabajaron clases y funciones

---
+++ app/src/main/java/com/example/clasesanidadasyinternaenum/MainActivity2.kt
@@ -0,0 +1,48 @@
+package com.example.clasesanidadaseinternasenum
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewParent
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
+import java.text.FieldPosition
+
+class MainActivity2 : AppCompatActivity() {
+    private var lvLenguajes: ListView? = null
+    private var tvSeleccion: TextView? = null
+    private var Lenguajes = arrayOf("Kotlin", "C#", "Java", "C++", "Php", "Vb.Net")
+    private val posiciones = arrayOf("1", "2", "3", "4", "5", "6")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        lvLenguajes = findViewById(R.id.Lv_Lenguajes)
+        tvSeleccion = findViewById(R.id.Tv_Seleccion)
+
+
+        var adaptador: ArrayAdapter<String> =
+            ArrayAdapter<String>(this, R.layout.list_item_claseprogramacion, Lenguajes)
+        lvLenguajes?.adapter = adaptador
+        lvLenguajes?.onItemClickListener = object : AdapterView.OnItemClickListener {
+            override fun onItemClick(
+                parent: AdapterView<*>?, view: View?, position: Int, id: Long
+            ) {
+                tvSeleccion?.text =
+                    "Posicion del lenguaje ${lvLenguajes?.getItemAtPosition(position)} es ${posiciones[position]}"
+
+            }
+
+        }
+    }
+
+    fun btn_Atras(Vista: View) {
+        val ventana2: Intent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(ventana2)
+
+    }
+}
